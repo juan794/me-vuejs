fd5aef5b40f864b6620974c36bce8d4ab2bce256
juan794 <juan794@users.noreply.github.com>
2022-07-09 08:35:21 +0000

    deploy: ff0e4ff551b44d5d706368ff6cf8d3043a40dc9f

--- _nuxt/1cd9942.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{164:function(n,l,t){"use strict";t.r(l);var e=t(32),component=Object(e.a)({},(function(){return(0,this._self._c)("h1",{staticClass:"text-3xl font-bold underline bg-slate-600"},[this._v("\n    Hello world!\n")])}),[],!1,null,null,null);l.default=component.exports}}]);
