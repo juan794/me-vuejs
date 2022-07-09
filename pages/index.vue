ff0e4ff551b44d5d706368ff6cf8d3043a40dc9f
Juan <juandavid794@gmail.com>
2022-07-09 16:34:30 +0800

    default

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,161 @@
 <template>
-    <h1 class="text-3xl font-bold underline bg-slate-600">
-        Hello world!
-    </h1>
+<div>
+    <header class="grid justify-center grid-flow-row bg-sky-900 px-8 pt-8 pb-4">
+        <div class="flex flex-col border-4 px-14 py-7">
+            <h1 class="text-gray-100 font-bold text-8xl text-center">Juan Guarnizo</h1>
+            <span class="bg-gray-100 text-sky-900 font-bold text-4xl text-center p-2 mt-4 mx-12 -mb-14">
+                Cybersecurity Engineer
+            </span>
+        </div>
+        <div class="text-center px-6 pt-12">
+            <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#PhD</span>
+            <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#SecOps</span>
+            <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Research</span>
+            <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Blockchain</span>
+            <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Cryptography</span>
+        </div>
+    </header>
+    <div aria-role="main">
+        <div class="container flex mx-auto px-56 gap-3 text-slate-700">
+            <div class="flex-auto w-2/5">
+                <div class="flex-auto p-4">
+                    <h1 class="flex-auto text-lg font-semibold text-sky-900">
+                        Profile
+                    </h1>
+                    <p class="text-base text-justify">
+                        Enthusiast cybersecurity engineer with four years of experience in the private and academic
+                        sectors.
+                        I am currently in the final year of my Ph.D. program, where I conduct research on blockchain and
+                        security applications in order to design trustless systems adopting data transparency.
+                    </p>
+                </div>
+                <div class="flex-auto p-4">
+                    <h1 class="flex-auto text-lg font-semibold text-sky-900">
+                        Contact
+                    </h1>
+                    <div>
+                        <span class="inline-block font-bold text-slate-900 text-base mb-2">Email:</span>
+                        juan_guarnizo [at] mymail.sutd.edu.sg
+                    </div>
+                    <div>
+                        <span class="inline-block font-bold text-slate-900 text-base mb-2">LinkedIn:</span>
+                        @juan794
+                    </div>
+                    <div>
+                        <span class="inline-block font-bold text-slate-900 text-base">Github:</span>
+                        @juan794
+                    </div>
+                </div>
+                <div class="flex-auto p-4">
+                    <h1 class="flex-auto text-lg font-semibold text-sky-900 mb-2">
+                        Education
+                    </h1>
+                    <div class="flex-auto mb-4">
+                        <h1 class="font-bold text-slate-900 text-base">PhD</h1>
+                        <p class="font-bold text-slate-600 text-base">Singapore University of Technology and Design</p>
+                        <p class="text-base text-justify">2017 - 2022</p>
+                    </div>
+                    <div class="flex-auto mb-4">
+                        <h1 class="font-bold text-slate-900 text-base">Master in Information Security</h1>
+                        <p class="font-bold text-slate-600 text-base">Universidad de los Andes</p>
+                        <p class="text-base text-justify">2013 - 2016</p>
+                    </div>
+                    <div class="flex-auto">
+                        <h1 class="font-bold text-slate-900 text-base">Bachelor in Computer Engineering</h1>
+                        <p class="font-bold text-slate-600 text-base">Universidad de los Llanos</p>
+                        <p class="text-base text-justify">2007 - 2012</p>
+                    </div>
+                </div>
+                <div class="flex-auto p-4">
+                    <h1 class="flex-auto text-lg font-semibold text-sky-900">
+                        Languages
+                    </h1>
+                    <div>
+                        <span class="font-bold text-slate-900 text-base mb-2">Spanish</span>
+                        - Native proficiency
+                    </div>
+                    <div>
+                        <span class="font-bold text-slate-900 text-base mb-2">English</span>
+                        - Professional proficiency
+                    </div>
+                </div>
+            </div>
+            <div class="flex-auto w-3/5">
+                <div class="flex-auto p-4">
+                    <h1 class="flex-auto text-lg font-semibold text-sky-900 mb-2">
+                        Experience
+                    </h1>
+                    <div class="flex-auto mb-4">
+                        <h1 class="font-bold text-slate-900 text-base">Research Assistant - STEE-SUTD Cyber Security Lab
+                        </h1>
+                        <p class="font-bold text-slate-600 text-base mb-2">at Singapore University of Technology and
+                            Design
+                            | Aug 2016 - Sep 2017</p>
+                        <p class="text-base text-justify">
+                            A cybersecurity researcher working on machine learning projects employing behavioral
+                            biometrics approaches to prevent insider attacks on corporations. I developed a multi-
+                            model authentication system that uses mouse movements and keystroke patterns to
+                            identify unauthorized users on devices.
+                        </p>
+                    </div>
+                    <div class="flex-auto mb-4">
+                        <h1 class="font-bold text-slate-900 text-base">Research Assistant - RESILIoT Lab</h1>
+                        <p class="font-bold text-slate-600 text-base mb-2">at Singapore University of Technology and
+                            Design
+                            | Mar 2016 - Aug 2017</p>
+                        <p class="text-base text-justify">
+                            A research member working on various cybersecurity projects, focusing primarily on the
+                            safety and security of devices in cyber physical systems. I participated in the creation of
+                            Honeypots to gain a better understanding of cyberattacks that target Internet of Things
+                            (IoT) devices.
+                        </p>
+                    </div>
+                    <div class="flex-auto mb-4">
+                        <h1 class="font-bold text-slate-900 text-base">Specialized Analyst - Information Security
+                            Department
+                        </h1>
+                        <p class="font-bold text-slate-600 text-base mb-2">at AV Villas Bank | Sep 2015 - Jan 2016</p>
+                        <p class="text-base text-justify">
+                            A security risk analyst for the Department of Information Security. The administrator
+                            of the platforms for vulnerability scanning and data loss prevention. I upgraded our
+                            QRadar scanner and developed a tool to extract daily scanning results directly from the
+                            embedded database and feed the company’s SIEM for events correlation and automatic
+                            metrics.
+                        </p>
+                    </div>
+                    <div class="flex-auto mb-4">
+                        <h1 class="font-bold text-slate-900 text-base">Professional Analyst - Technologies for the
+                            Information Security
+                        </h1>
+                        <p class="font-bold text-slate-600 text-base mb-2">at BBVA Colombia Bank | Mar 2015 - Aug 2016
+                        </p>
+                        <p class="text-base text-justify">
+                            A cybersecurity engineer responsible for the administration and management of a variety
+                            of IT and security platforms, including firewalls, web proxy, vulnerability scanners, and
+                            ATM protection tools, within the Technologies for the Information Security department.
+                        </p>
+                    </div>
+                    <div class="flex-auto mb-4">
+                        <h1 class="font-bold text-slate-900 text-base">Senior Consultant</h1>
+                        <p class="font-bold text-slate-600 text-base mb-2">at Krypto Colombia | Nov 2014 - Feb 2015</p>
+                        <p class="text-base text-justify">
+                            As an internal outsourcing consultant for the Technologies for the Information Security
+                            department, I was responsible for the administration and management of numerous IT
+                            and security platforms, such as firewalls, web proxy, vulnerability scanners, and ATM
+                            protection tools. I joined the team after a cyberattack in which hackers stole COP$1.024
+                            million by hacking ATMs. I was responsible for restoring and enhancing ATM security
+                            using several tools to ensure that over a million devices were fully functional and
+                            protected.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
